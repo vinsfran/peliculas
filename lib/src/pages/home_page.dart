46ec3677ded18516a5cffa2e1cead8bb1ea4e5aa
Vicente Insfran <vicente.insfran@asuncion.gov.py>
2021-02-26 15:35:35 -0300

    98. Widget personalizado - CardSwiper

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:peliculas/src/widgets/card_swiper_widget.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -27,23 +26,8 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _swiperTarjetas() {
-    return Container(
-      padding: EdgeInsets.only(top: 10.0),
-      width: double.infinity,
-      height: 300.0,
-      child: Swiper(
-        layout: SwiperLayout.STACK,
-        itemWidth: 200.0,
-        itemBuilder: (BuildContext context, int index) {
-          return Image.network(
-            "http://via.placeholder.com/350x150",
-            fit: BoxFit.fill,
-          );
-        },
-        itemCount: 3,
-        // pagination: new SwiperPagination(),
-        // control: new SwiperControl(),
-      ),
+    return CarSwiperWidget(
+      peliculas: [1, 2, 3, 4, 5],
     );
   }
 }
