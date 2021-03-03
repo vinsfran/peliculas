c718294784d751247ca04208833c2b11d99d360a
Vicente Insfran <vicente.insfran@asuncion.gov.py>
2021-03-03 15:03:20 -0300

    120. Search Delegate

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -18,7 +18,9 @@ class HomePage extends StatelessWidget {
           actions: <Widget>[
             IconButton(
               icon: Icon(Icons.search),
-              onPressed: () {},
+              onPressed: () {
+                // showSearch(context: null, delegate: null)
+              },
             )
           ],
         ),
